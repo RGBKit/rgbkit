95a47a1d9eaa859b581809bd32ee8075937e5b59
Sajyd <mounib_s@epitech.eu>
2020-08-12 18:24:48 +0200

    RGBKit NZXT Provider
    Implemented NZXTSharp to add support of KrakenX3 devices in RGBKit. Only works for KrakenX3 devices for now (x53, x63, x73), might add KrakenX2 and Hue Later

---
+++ src/RGBKit.Providers.NZXT/NZXTDeviceProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using RGBKit.Providers.NZXT;
+
+namespace RGBKit.Core
+{
+    /// <summary>
+    /// The NZXT RGB Kit extension methods
+    /// </summary>
+    public static class NZXTDeviceProviderExtensions
+    {
+        /// <summary>
+        /// Uses the NZXT device provider
+        /// </summary>
+        /// <param name="rgbKit">The RGB Kit instance to add the provider to</param>
+        public static void UseNZXT(this IRGBKitService rgbKit)
+        {
+            rgbKit.AddProvider(new NZXTDeviceProvider());
+        }
+    }
+}
